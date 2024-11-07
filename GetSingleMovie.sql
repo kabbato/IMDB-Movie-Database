857cece27871bbe795dd122fdce36d50230cd9e9
Krish Abbato <160180758+kabbato@users.noreply.github.com>
2024-11-07 00:12:47 -0500

    Update GetSingleMovie.sql

--- GetSingleMovie.sql
+++ GetSingleMovie.sql
@@ -1,5 +1,5 @@
 Select Title, Description, Director, Actors, Rating 
-From dbo.[IMDB-Movie-Data] 
+From [IMDB-Movies].dbo.[IMDB-Movie-Data] 
 Where Rank = '1'
 
 -- Enter the rank (ID) of the movie you want to look at
